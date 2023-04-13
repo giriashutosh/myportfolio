382e87d6f529965e8e8b2c3cbd07bb4ec01ba258
ramankumar12995 <ramankumar12995@gmail.com>
2023-04-13 17:41:01 +0530

    added floating div

--- src/components/FloatingDiv/FloatingDiv.jsx
+++ src/components/FloatingDiv/FloatingDiv.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
-
-const FloatingDiv = ({image, txt1, txt2}) => {
+import './Floating.css'
+const FloatingDiv = ({img, txt1, txt2}) => {
   return (
     <div className='floatingdiv'>
-
+      <img src={img} alt=''/>
+      <span>
+        {txt1}
+        <br />
+        {txt2}
+      </span>
     </div>
   )
 }
--- src/components/Intro/intro.jsx
+++ src/components/Intro/intro.jsx
@@ -8,32 +8,40 @@ import boy from "../../img/boy.png";
 import glassesimoji from "../../img/glassesimoji.png";
 import thumbup from "../../img/thumbup.png";
 import crown from "../../img/crown.png";
+import FloatingDiv from '../FloatingDiv/FloatingDiv';
 
 const Intro = () => {
   return (
     <div className='intro'>
-          <div className='i-left'>
-              <div className='i-name'>
-                  <span>Hy! I Am</span>
-                  <span>Ashutosh Giri</span>
-                  <span>
-                    Software Developer with high level of experience in web designing
-                    and development in React and 3D-Modelling, FEA Software, DSA, System Design, producting the Quality work
-                </span>
-              </div>
-              <button className='button i-button'>Hire Me</button>
-              <div className='i-icons '>
-                  <img src={Github} alt='' />
-                  <img src={LinkedIn} alt='' />
-                  
-              </div>
-          </div>
-          
-          <div className='i-right'>
-              <img src={Vector1} alt='' />
-              <img src={Vector2} alt='' />
-              <img src={boy} alt="" />
-          </div>
+      <div className='i-left'>
+        <div className='i-name'>
+          <span>Hy! I Am</span>
+          <span>Ashutosh Giri</span>
+          <span>
+            Software Developer with high level of experience in web designing
+            and development in React and 3D-Modelling, FEA Software, DSA, System Design, producting the Quality work
+          </span>
+        </div>
+        <button className='button i-button'>Hire Me</button>
+        <div className='i-icons '>
+          <img src={Github} alt='' />
+          <img src={LinkedIn} alt='' />
+
+        </div>
+      </div>
+
+      <div className='i-right'>
+        <img src={Vector1} alt='' />
+        <img src={Vector2} alt='' />
+        <img src={boy} alt="" />
+        <img src={glassesimoji} alt=''/>
+        <div className='floating-div'>
+          <FloatingDiv img={crown} txt1="Web" txt2="Developer" />
+        </div>
+        <div className='floating-div'>
+          <FloatingDiv img={thumbup} txt1="Best" txt2="DSA Solving Award" />
+        </div>
+      </div>
     </div>
   )
 }
