15b62f8a9b2a869baa8ebdf32e8dd5dafb3b9978
ramankumar12995 <ramankumar12995@gmail.com>
2023-04-13 12:46:36 +0530

    added Navbar

---
+++ src/components/Navbar/navbar.jsx
@@ -0,0 +1,22 @@
+import './navbar.css'
+export const Navbar = () => {
+    return (
+        <div className="n-wrapper">
+            <div className="n-left">
+                <div className="n-name">Ashutosh Giri</div>
+                <span>toggle</span>
+            </div>
+            <div className="n-right">
+                <div className="n-list">
+                    <ul style={{listStyleType: "none"}}>
+                        <li>Home</li>
+                        <li>Projects</li>
+                        <li>Experience</li>
+                        <li>Portfolio</li>
+                    </ul>
+                </div>
+                <button className="button n-button">Contact Me</button>
+            </div>
+        </div>
+    )
+}
